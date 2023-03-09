4cf0213e732bba596f5292db6844f0fb3b48f144
unknown <Vicky>
2023-03-09 09:17:14 +0100

    Modifico titulo desde master

--- limitesjuego.php
+++ limitesjuego.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Adivina el número secreto</title>
+        <title>Adivina el secret number</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="stylesheet.css">
     </head>
